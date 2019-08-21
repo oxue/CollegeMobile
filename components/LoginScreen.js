ed3a374560d5ff14a25dba1c42e24ce3068bbc0a
Oliver Xu <oxu.wex@gmail.com>
2019-08-21 09:52:22 -0700

    add react native navigation modules

---
+++ components/LoginScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {
+  Text
+} from 'react-native';
+
+class LoginScreen extends React.Component {
+  render() {
+    return (
+      <Text>
+        LoginScreen
+      </Text>
+    );
+  }
+}
+
+export default LoginScreen;
---
+++ components/SignupScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {
+  Text
+} from 'react-native';
+
+class SignupScreen extends React.Component {
+  render() {
+    return (
+      <Text>
+        SignupScreen
+      </Text>
+    );
+  }
+}
+
+export default SignupScreen;
